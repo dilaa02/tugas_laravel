120aaff5aae9cadc5eb7bae5f9f24d48b72e2eb3
Dirokul Adli <149842338+dilaa02@users.noreply.github.com>
2024-07-03 11:44:29 +0700

    dasbor ni..

--- resources/views/pages/dashboard-ecommerce-dashboard.blade.php
+++ resources/views/pages/dashboard-ecommerce-dashboard.blade.php
@@ -127,7 +127,7 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-lg-8">
+                <div class="col-lg-12">
                     <div class="card">
                         <div class="card-header">
                             <h4>Budget vs Sales</h4>
@@ -138,7 +138,9 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4">
+            </div>
+            <div class="row">
+                <div class="col-md-4">
                     <div class="card gradient-bottom">
                         <div class="card-header">
                             <h4>Top 5 Products</h4>
@@ -284,7 +286,7 @@
                                 </li>
                             </ul>
                         </div>
-                        <div class="card-footer d-flex justify-content-center pt-3">
+                        <div class="card-footer d-flex justify-content-center pt-2">
                             <div class="budget-price justify-content-center">
                                 <div class="budget-price-square bg-primary"
                                     data-width="20"></div>
@@ -298,14 +300,12 @@
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-8">
                     <div class="card">
                         <div class="card-header">
                             <h4>Best Products</h4>
                         </div>
-                        <div class="card-body">
+                        <div class="card-body my-3">
                             <div class="owl-carousel owl-theme"
                                 id="products-carousel">
                                 <div>
@@ -382,214 +382,10 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4>Top Countries</h4>
-                        </div>
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <div class="text-title mb-2">July</div>
-                                    <ul class="list-unstyled list-unstyled-border list-unstyled-noborder mb-0">
-                                        <li class="media">
-                                            <span class='flag-icon flag-icon-id'></span>
-                                            <div class="media-body ml-3">
-                                                <div class="media-title">Indonesia</div>
-                                                <div class="text-small text-muted">3,282 <i
-                                                        class="fas fa-caret-down text-danger"></i></div>
-                                            </div>
-                                        </li>
-                                        <li class="media">
-                                            <span class='flag-icon flag-icon-my'></span>
-                                            <div class="media-body ml-3">
-                                                <div class="media-title">Malaysia</div>
-                                                <div class="text-small text-muted">2,976 <i
-                                                        class="fas fa-caret-down text-danger"></i></div>
-                                            </div>
-                                        </li>
-                                        <li class="media">
-                                            <span class='flag-icon flag-icon-us'></span>
-
-                                            <div class="media-body ml-3">
-                                                <div class="media-title">United States</div>
-                                                <div class="text-small text-muted">1,576 <i
-                                                        class="fas fa-caret-up text-success"></i></div>
-                                            </div>
-                                        </li>
-                                    </ul>
+                        <div class="card-footer d-flex justify-content-center pt-3">
+                            <div class="budget-price justify-content-center">
+                                <div class="budget-price-square bg-warning" data-width="1000">
                                 </div>
-                                <div class="col-sm-6 mt-sm-0 mt-4">
-                                    <div class="text-title mb-2">August</div>
-                                    <ul class="list-unstyled list-unstyled-border list-unstyled-noborder mb-0">
-                                        <li class="media">
-                                            <span class='flag-icon flag-icon-id'></span>
-                                            <div class="media-body ml-3">
-                                                <div class="media-title">Indonesia</div>
-                                                <div class="text-small text-muted">3,486 <i
-                                                        class="fas fa-caret-up text-success"></i></div>
-                                            </div>
-                                        </li>
-                                        <li class="media">
-                                            <span class='flag-icon flag-icon-ps'></span>
-
-                                            <div class="media-body ml-3">
-                                                <div class="media-title">Palestine</div>
-                                                <div class="text-small text-muted">3,182 <i
-                                                        class="fas fa-caret-up text-success"></i></div>
-                                            </div>
-                                        </li>
-                                        <li class="media">
-                                            <span class='flag-icon flag-icon-de'></span>
-
-                                            <div class="media-body ml-3">
-                                                <div class="media-title">Germany</div>
-                                                <div class="text-small text-muted">2,317 <i
-                                                        class="fas fa-caret-down text-danger"></i></div>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4>Invoices</h4>
-                            <div class="card-header-action">
-                                <a href="#"
-                                    class="btn btn-danger">View More <i class="fas fa-chevron-right"></i></a>
-                            </div>
-                        </div>
-                        <div class="card-body p-0">
-                            <div class="table-responsive table-invoice">
-                                <table class="table-striped table">
-                                    <tr>
-                                        <th>Invoice ID</th>
-                                        <th>Customer</th>
-                                        <th>Status</th>
-                                        <th>Due Date</th>
-                                        <th>Action</th>
-                                    </tr>
-                                    <tr>
-                                        <td><a href="#">INV-87239</a></td>
-                                        <td class="font-weight-600">Kusnadi</td>
-                                        <td>
-                                            <div class="badge badge-warning">Unpaid</div>
-                                        </td>
-                                        <td>July 19, 2018</td>
-                                        <td>
-                                            <a href="#"
-                                                class="btn btn-primary">Detail</a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td><a href="#">INV-48574</a></td>
-                                        <td class="font-weight-600">Hasan Basri</td>
-                                        <td>
-                                            <div class="badge badge-success">Paid</div>
-                                        </td>
-                                        <td>July 21, 2018</td>
-                                        <td>
-                                            <a href="#"
-                                                class="btn btn-primary">Detail</a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td><a href="#">INV-76824</a></td>
-                                        <td class="font-weight-600">Muhamad Nuruzzaki</td>
-                                        <td>
-                                            <div class="badge badge-warning">Unpaid</div>
-                                        </td>
-                                        <td>July 22, 2018</td>
-                                        <td>
-                                            <a href="#"
-                                                class="btn btn-primary">Detail</a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td><a href="#">INV-84990</a></td>
-                                        <td class="font-weight-600">Agung Ardiansyah</td>
-                                        <td>
-                                            <div class="badge badge-warning">Unpaid</div>
-                                        </td>
-                                        <td>July 22, 2018</td>
-                                        <td>
-                                            <a href="#"
-                                                class="btn btn-primary">Detail</a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td><a href="#">INV-87320</a></td>
-                                        <td class="font-weight-600">Ardian Rahardiansyah</td>
-                                        <td>
-                                            <div class="badge badge-success">Paid</div>
-                                        </td>
-                                        <td>July 28, 2018</td>
-                                        <td>
-                                            <a href="#"
-                                                class="btn btn-primary">Detail</a>
-                                        </td>
-                                    </tr>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card card-hero">
-                        <div class="card-header">
-                            <div class="card-icon">
-                                <i class="far fa-question-circle"></i>
-                            </div>
-                            <h4>14</h4>
-                            <div class="card-description">Customers need help</div>
-                        </div>
-                        <div class="card-body p-0">
-                            <div class="tickets-list">
-                                <a href="#"
-                                    class="ticket-item">
-                                    <div class="ticket-title">
-                                        <h4>My order hasn't arrived yet</h4>
-                                    </div>
-                                    <div class="ticket-info">
-                                        <div>Laila Tazkiah</div>
-                                        <div class="bullet"></div>
-                                        <div class="text-primary">1 min ago</div>
-                                    </div>
-                                </a>
-                                <a href="#"
-                                    class="ticket-item">
-                                    <div class="ticket-title">
-                                        <h4>Please cancel my order</h4>
-                                    </div>
-                                    <div class="ticket-info">
-                                        <div>Rizal Fakhri</div>
-                                        <div class="bullet"></div>
-                                        <div>2 hours ago</div>
-                                    </div>
-                                </a>
-                                <a href="#"
-                                    class="ticket-item">
-                                    <div class="ticket-title">
-                                        <h4>Do you see my mother?</h4>
-                                    </div>
-                                    <div class="ticket-info">
-                                        <div>Syahdan Ubaidillah</div>
-                                        <div class="bullet"></div>
-                                        <div>6 hours ago</div>
-                                    </div>
-                                </a>
-                                <a href="features-tickets.html"
-                                    class="ticket-item ticket-more">
-                                    View All <i class="fas fa-chevron-right"></i>
-                                </a>
                             </div>
                         </div>
                     </div>
